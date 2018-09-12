c93d06ed7319393201f98af014b5a469f4575855
Aayush <nandkeolyar.aayush@gmail.com>
2018-09-12 20:58:31 +0530

    static changes

--- school_project/settings.py
+++ school_project/settings.py
@@ -15,10 +15,7 @@ import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_DIR = os.path.join(BASE_DIR, "static")
-MEDIA_DIR = os.path.join(BASE_DIR, "media")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT =  os.path.join(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
